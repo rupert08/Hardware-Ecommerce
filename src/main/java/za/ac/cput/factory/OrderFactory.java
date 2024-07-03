9d42851d1ec3e53f5cdc025cbaa0f2b56726c59f
Mpumlwana Lakhikhaya <220204594@mycput.ac.za>
2024-07-03 20:42:46 +0200

    These are my Factory classes and their tests.

---
+++ src/main/java/za/ac/cput/factory/OrderFactory.java
@@ -0,0 +1,29 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.Order;
+import za.ac.cput.domain.OrderItem;
+import za.ac.cput.util.Helper;
+import java.time.LocalDate;
+import java.util.Set;
+
+public class OrderFactory {
+
+    public static Order buildOrder(long userID, LocalDate orderDate, Address address, float totalAmount, String orderStatus, Set<OrderItem> orderItems) {
+        if (Helper.isNullOrEmpty(String.valueOf(userID)) || Helper.isNullOrEmpty(String.valueOf(orderDate))
+                || Helper.isNullOrEmpty(String.valueOf(address)) || Helper.isNullOrEmpty(String.valueOf(totalAmount))
+                || Helper.isNullOrEmpty(orderStatus) || Helper.isNullOrEmpty(orderItems.toString())) {
+            return null;
+        }
+
+        return new Order.Builder()
+                .setUserID(userID)
+                .setOrderDate(orderDate)
+                .setDeliveryAddress(address)
+                .setTotalAmount(totalAmount)
+                .setOrderStatus(orderStatus)
+                .setOrderItems(orderItems)
+                .build();
+    }
+}
+
